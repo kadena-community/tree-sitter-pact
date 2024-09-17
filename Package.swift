aaa85ab18848b2ac3f37a6a630e2b13d34bee137
Salama Ashoush <salamaashoush@gmail.com>
2024-09-17 22:41:07 +0200

    :package: fix other bindings

--- Package.swift
+++ Package.swift
@@ -4,44 +4,56 @@ import PackageDescription
 let package = Package(
     name: "TreeSitterPact",
     products: [
-        .library(name: "TreeSitterPact", targets: ["TreeSitterPact"]),
+        .library(name: "TreeSitterPact", targets: ["TreeSitterPact"])
     ],
-    dependencies: [],
+    dependencies: [
+        .package(url: "https://github.com/ChimeHQ/SwiftTreeSitter", from: "0.8.0")
+    ],
     targets: [
-        .target(name: "TreeSitterPact",
-                path: ".",
-                exclude: [
-                    "Cargo.toml",
-                    "Makefile",
-                    "binding.gyp",
-                    "bindings/c",
-                    "bindings/go",
-                    "bindings/node",
-                    "bindings/python",
-                    "bindings/rust",
-                    "prebuilds",
-                    "grammar.js",
-                    "package.json",
-                    "package-lock.json",
-                    "pyproject.toml",
-                    "setup.py",
-                    "test",
-                    "examples",
-                    ".editorconfig",
-                    ".github",
-                    ".gitignore",
-                    ".gitattributes",
-                    ".gitmodules",
-                ],
-                sources: [
-                    "src/parser.c",
-                    // NOTE: if your language has an external scanner, add it here.
-                ],
-                resources: [
-                    .copy("queries")
-                ],
-                publicHeadersPath: "bindings/swift",
-                cSettings: [.headerSearchPath("src")])
+        .target(
+            name: "TreeSitterPact",
+            path: ".",
+            exclude: [
+                "Cargo.toml",
+                "Makefile",
+                "binding.gyp",
+                "bindings/c",
+                "bindings/go",
+                "bindings/node",
+                "bindings/python",
+                "bindings/rust",
+                "prebuilds",
+                "grammar.js",
+                "package.json",
+                "package-lock.json",
+                "pyproject.toml",
+                "setup.py",
+                "test",
+                "examples",
+                ".editorconfig",
+                ".github",
+                ".gitignore",
+                ".gitattributes",
+                ".gitmodules",
+            ],
+            sources: [
+                "src/parser.c"
+                // NOTE: if your language has an external scanner, add it here.
+            ],
+            resources: [
+                .copy("queries")
+            ],
+            publicHeadersPath: "bindings/swift",
+            cSettings: [.headerSearchPath("src")]
+        ),
+        .testTarget(
+            name: "TreeSitterPactTests",
+            dependencies: [
+                "SwiftTreeSitter",
+                "TreeSitterPact",
+            ],
+            path: "bindings/swift/TreeSitterPactTests"
+        ),
     ],
     cLanguageStandard: .c11
 )
